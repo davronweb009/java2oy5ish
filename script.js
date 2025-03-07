7a0af6c74febe385c4ba8e6a5d1c77e40d7ef077
davronweb009 <davronweb07@mail.ru>
2025-03-07 16:55:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const taskInput = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('addTaskButton');
+const tasks = document.getElementById('tasks');
+const imgg = document.getElementById('imgg');
+const body = document.body;
+
+
+addTaskButton.addEventListener('click', () => {
+    const taskText = taskInput.value.trim();
+    if (taskText === '') return;
+
+    const taskDiv = document.createElement('div');
+    taskDiv.textContent = taskText;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'NOTE#1';
+
+
+    deleteButton.addEventListener('click', () => {
+        taskDiv.remove();
+    });
+
+    taskDiv.appendChild(deleteButton);
+    tasks.appendChild(taskDiv);
+    taskInput.value = '';
+});
+
+
+imgg.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+    imgg.innerHTML = body.classList.contains('dark-theme') ? '☀' : '🌙';
+});
